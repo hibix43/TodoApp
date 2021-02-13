a1aada4764506e647f8dc4a78ebd50f3efc0d0c3
hibix43 <hibix43knzw+github@gmail.com>
2021-02-13 21:38:03 +0900

    WIP

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,6 +1,42 @@
+import React from 'react';
 import styled from 'styled-components';
 
-// TODO: display: none を使わずにデザイン
-const Checkbox = styled.input.attrs({ type: 'checkbox' })``;
+type Props = {
+  className: string;
+  label: string;
+  name: string;
+  value: boolean;
+  changeValue: (name: string, value: boolean) => void;
+};
 
-export default Checkbox;
+const Component: React.VFC<Props> = ({
+  className,
+  label,
+  name,
+  value,
+  changeValue
+}) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    changeValue(event.target.name, event.target.checked);
+  };
+
+  return (
+    <span className={className}>
+      <label className={`${className}__label`}>
+        <input
+          className={`${className}__input`}
+          type="checkbox"
+          name={name}
+          checked={value}
+          onChange={handleChange}
+        />
+        {label}
+      </label>
+    </span>
+  );
+};
+
+// TODO
+const StyledComponent = styled(Component)``;
+
+export const Checkbox = StyledComponent;

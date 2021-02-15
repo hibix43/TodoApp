fed1c5b019e7de89dd9a255d8a76d128ab147755
hibix43 <hibix43knzw+github@gmail.com>
2021-02-15 21:36:24 +0900

    Todo コンポーネントにスタイルをつけた

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import styled from 'styled-components';
+import { StyledProps } from './../../types';
 import Button from '../Button/Button';
 
 type Props = {
@@ -6,19 +8,50 @@ type Props = {
   title: string;
 };
 
-export const Todo: React.VFC<Props> = ({ check, title }) => {
+const Component: React.VFC<Props & StyledProps> = ({
+  className,
+  check,
+  title
+}) => {
   return (
-    <div>
+    <div className={`${className}`}>
       <Button
+        className={`${className}__check`}
         imgUrl={
           check
             ? './assets/images/checked.svg'
             : './assets/images/unchecked.svg'
         }
       />
-      {title}
-      <Button imgUrl={'./assets/images/edit.svg'} />
-      <Button imgUrl={'./assets/images/delete.svg'} />
+      <span className={`${className}__title`}>{title}</span>
+      <Button
+        className={`${className}__edit`}
+        imgUrl={'./assets/images/edit.svg'}
+      />
+      <Button
+        className={`${className}__delete`}
+        imgUrl={'./assets/images/delete.svg'}
+      />
     </div>
   );
 };
+
+const StyledComponent = styled(Component)`
+  display: flex;
+  flex-direction: row;
+  * {
+    vertical-align: middle;
+  }
+  &__check,
+  &__edit {
+    margin-right: 16px;
+  }
+  &__title {
+    font-size: 1.5rem;
+  }
+  &__edit {
+    margin-left: auto;
+  }
+`;
+
+export const Todo = StyledComponent;

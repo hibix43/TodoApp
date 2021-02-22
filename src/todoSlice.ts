97ec8025ca59564be48829c065cc9236021c0373
hibix43 <hibix43knzw+github@gmail.com>
2021-02-22 21:47:39 +0900

    最低限の todoSlice を作成

---
+++ src/todoSlice.ts
@@ -0,0 +1,27 @@
+import { createSlice, PayloadAction, nanoid } from '@reduxjs/toolkit';
+import { Todo } from './types';
+
+export type State = {
+  todos: Todo[];
+};
+
+const initialState: State = {
+  todos: []
+};
+
+const slice = createSlice({
+  name: 'todo',
+  initialState,
+  reducers: {
+    addTodo: (state: State, action: PayloadAction<string>) => {
+      const newTodo = { id: nanoid(), title: action.payload, checked: false };
+      state.todos.push(newTodo);
+    },
+    changeChecked: (state: State, action: PayloadAction<string>) => {
+      const targetTodo = state.todos.find((todo) => todo.id === action.payload);
+      if (targetTodo) {
+        targetTodo.checked = true;
+      }
+    }
+  }
+});
